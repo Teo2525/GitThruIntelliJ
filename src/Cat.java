d79630ba46e9f12db8ff9805ffa879d91e1ef2cc
Telman <telman.weber39@gmail.com>
2022-05-19 17:09:05 +0400

    made some implementation

--- src/Cat.java
+++ src/Cat.java
@@ -1,5 +1,7 @@
 public class Cat {
     public static void main(String[] args) {
         System.out.println("cat1");
+        System.out.println("cat3");
+        System.out.println("cat4");
     }
 }
