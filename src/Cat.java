fc4ad677a82a037393db1eb6e066d9e1a5c7a367
Teo2525 <97795953+Teo2525@users.noreply.github.com>
2022-05-19 18:05:42 +0400

    Update Cat.java
    another lines

--- src/Cat.java
+++ src/Cat.java
@@ -3,5 +3,7 @@ public class Cat {
         System.out.println("cat1");
         System.out.println("cat3");
         System.out.println("cat4");
+         System.out.println("cat8");
+        System.out.println("cat6");
     }
 }
